1f0dd69b4e941eca66d061c6e8cc543e5b1de8d6
leabeno <74978353+leabeno@users.noreply.github.com>
2024-02-19 21:32:23 +0100

    Update main.py
    code clarity

--- main.py
+++ main.py
@@ -86,66 +86,113 @@ def hide_or_show_sidebar_while_clicking_on_sidebar_button(n_clicks):
      Input('contract', 'value'),
      Input('payment_method', 'value')]
 )
-def update_graph(gender_value, senior_citizen_value, phone_service_value, internet_service_value, contract_value,
-                 payment_method_value):
+def update_graph(gender_value, senior_citizen_value, 
+                 phone_service_value, internet_service_value, 
+                 contract_value, payment_method_value):
 
     data_copy = data.copy()
 
     if gender_value is not None:
-        data_copy = data_copy[data_copy['gender'] == GetSidebarElementsValue.get_sidebar_value(gender_value, "gender")]
-    if senior_citizen_value is not None:
-        data_copy = data_copy[data_copy['SeniorCitizen'] == GetSidebarElementsValue.get_sidebar_value(senior_citizen_value, "senior_citizen")]
-    if phone_service_value is not None:
-        data_copy = data_copy[data_copy['PhoneService'] == GetSidebarElementsValue.get_sidebar_value(phone_service_value, "phone_service")]
-    if internet_service_value is not None:
-        data_copy = data_copy[data_copy['InternetService'] == GetSidebarElementsValue.get_sidebar_value(internet_service_value, "internet_service")]
-    if contract_value is not None:
-        data_copy = data_copy[data_copy['Contract'] == GetSidebarElementsValue.get_sidebar_value(contract_value, "contract")]
-    if payment_method_value is not None:
-        data_copy = data_copy[data_copy['PaymentMethod'] == GetSidebarElementsValue.get_sidebar_value(payment_method_value, "payment_method")]
+        gender_filter = GetSidebarElementsValue.get_sidebar_value(
+            gender_value, "gender")
+        data_copy = data_copy[data_copy['gender'] == gender_filter]
 
-    figure1 = px.scatter(data_copy, x='MonthlyCharges', y='TotalCharges', color='gender',
-                     title='Total Charges vs Monthly Charges',
-                     labels={'MonthlyCharges': 'Monthly Charges', 'TotalCharges': 'Total Charges'})
+    if senior_citizen_value is not None:
+        senior_filter = GetSidebarElementsValue.get_sidebar_value(
+            senior_citizen_value, "senior_citizen")
+        data_copy = data_copy[data_copy['SeniorCitizen'] == senior_filter]
+
+    if phone_service_value is not None:
+        phone_filter = GetSidebarElementsValue.get_sidebar_value(
+            phone_service_value, "phone_service")
+        data_copy = data_copy[data_copy['PhoneService'] == phone_filter]
+
+    if internet_service_value is not None:
+        internet_filter = GetSidebarElementsValue.get_sidebar_value(
+            internet_service_value, "internet_service")
+        data_copy = data_copy[data_copy['InternetService'] == internet_filter]
+
+    if contract_value is not None:
+        contract_filter = GetSidebarElementsValue.get_sidebar_value(
+            contract_value, "contract")
+        data_copy = data_copy[data_copy['Contract'] == contract_filter]
+
+    if payment_method_value is not None:
+        payment_filter = GetSidebarElementsValue.get_sidebar_value(
+            payment_method_value, "payment_method")
+        data_copy = data_copy[
+            data_copy['PaymentMethod'] == payment_filter]
+
+    figure1 = px.scatter(
+        data_copy, x='MonthlyCharges', y='TotalCharges', color='gender',
+        title='Total Charges vs Monthly Charges',
+        labels={'MonthlyCharges': 'Monthly Charges', 
+                'TotalCharges': 'Total Charges'}
+    )
 
     churn_counts = data_copy['Churn'].value_counts()
     colors = ['lightblue', 'lightgreen']
-    figure2 = go.Figure(
-        data=[go.Pie(labels=churn_counts.index, values=churn_counts.values, hole=.3, marker_colors=colors)])
+    figure2 = go.Figure(data=[go.Pie(
+        labels=churn_counts.index, 
+        values=churn_counts.values, 
+        hole=.3, 
+        marker_colors=colors
+    )])
     figure2.update_traces(textinfo='percent+label', textfont_size=14)
     figure2.update_layout(title='Churn Distribution')
 
-    figure3 = px.histogram(data_copy, x='MultipleLines', color='MultipleLines', title='Multiple Lines Distribution',
-                           pattern_shape="MultipleLines", barmode='stack')
+    figure3 = px.histogram(
+        data_copy, x='MultipleLines', color='MultipleLines', 
+        title='Multiple Lines Distribution',
+        pattern_shape="MultipleLines", barmode='stack'
+    )
     figure3.update_traces(texttemplate='%{value}', textposition='outside')
     figure3.update_layout(legend_title_text='Multiple Lines')
 
-    streaming_tv_avg_charges = data_copy.groupby('StreamingTV')['MonthlyCharges'].mean().reset_index()
-    figure4 = px.bar(streaming_tv_avg_charges, x='StreamingTV', y='MonthlyCharges',
-                     text='MonthlyCharges', color='StreamingTV',
-                     title='Charges Mensuelles Moyennes par Statut de Streaming TV')
+    streaming_tv_avg_charges = data_copy.groupby(
+        'StreamingTV')['MonthlyCharges'].mean().reset_index()
+    figure4 = px.bar(
+        streaming_tv_avg_charges, x='StreamingTV', y='MonthlyCharges',
+        text='MonthlyCharges', color='StreamingTV',
+        title='Charges Mensuelles Moyennes par Statut de Streaming TV'
+    )
     figure4.update_traces(texttemplate='%{text:.2f}', textposition='outside')
-    figure4.update_layout(xaxis_title="Statut de Streaming TV",
-                          yaxis_title="Charges Mensuelles Moyennes ($)")
+    figure4.update_layout(
+        xaxis_title="Statut de Streaming TV",
+        yaxis_title="Charges Mensuelles Moyennes ($)"
+    )
 
-    # Streaming Movies Distribution - Box Plot
-    figure5 = px.box(data_copy, x='StreamingMovies', y='MonthlyCharges', color='StreamingMovies',
-                     title='Distribution des Charges Mensuelles par Catégorie de Streaming de Films')
-    figure5.update_layout(xaxis_title="Catégorie de Streaming de Films",
-                          yaxis_title="Charges Mensuelles ($)")
+    figure5 = px.box(
+        data_copy, x='StreamingMovies', y='MonthlyCharges', 
+        color='StreamingMovies',
+        title='Distribution des Charges Mensuelles par Catégorie de Streaming de Films'
+    )
+    figure5.update_layout(
+        xaxis_title="Catégorie de Streaming de Films",
+        yaxis_title="Charges Mensuelles ($)"
+    )
 
-    figure6 = px.box(data_copy, x='InternetService', y='MonthlyCharges', color='InternetService',
-                     title="Distribution des Charges Mensuelles par Type de Service Internet")
+    figure6 = px.box(
+        data_copy, x='InternetService', y='MonthlyCharges', 
+        color='InternetService',
+        title="Distribution des Charges Mensuelles par Type de Service Internet"
+    )
 
     online_security_counts = data_copy['OnlineSecurity'].value_counts()
-    figure7 = px.pie(names=online_security_counts.index, values=online_security_counts.values,
-                     title='Online Security Distribution')
+    figure7 = px.pie(
+        names=online_security_counts.index, 
+        values=online_security_counts.values,
+        title='Online Security Distribution'
+    )
 
-    # Distribution de la sauvegarde en ligne
     online_backup_counts = data_copy['OnlineBackup'].value_counts()
-    figure8 = px.bar(x=online_backup_counts.index, y=online_backup_counts.values, title='Online Backup Distribution')
+    figure8 = px.bar(
+        x=online_backup_counts.index, y=online_backup_counts.values, 
+        title='Online Backup Distribution'
+    )
 
     return figure1, figure2, figure3, figure4, figure5, figure6, figure7, figure8
+
 
 # endregion
 
